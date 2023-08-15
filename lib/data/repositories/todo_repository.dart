c3e3b907fd4d8b36e9ddce7b8893935ce3a6896a
TNQT14 <2002quangthai@gmail.com>
2023-08-15 14:02:17 +0700

    TodoRepositoryImpl

--- lib/data/repositories/todo_repository.dart
+++ lib/data/repositories/todo_repository.dart
@@ -5,5 +5,5 @@ abstract class TodoRepository{
   Future<void> addTodo(Todo todo);
   Future<void> deleteTodo(Todo todo);
   Future<void> updateTodo(Todo todo);
-  Future<void> deleteAllTodo();
+  Future<void> deleteAllTodos();
 }
--- lib/data/repositories/todo_repository_impl.dart
+++ lib/data/repositories/todo_repository_impl.dart
@@ -1,2 +1,55 @@
+import 'package:hive/hive.dart';
 import 'package:todo_app/data/repositories/todo_repository.dart';
+import 'package:todo_app/model/todo.dart';
 
+import '../data_source/local/todo_db_service.dart';
+import '../data_source/network/todo_firestore_service.dart';
+
+class TodoRepositoryImpl implements TodoRepository{
+  late TodoDbService _todoDbService;
+  late TodoFirestoreService _todoFirestoreService;
+
+  late Box box;
+
+  TodoProvider(){
+    _initHive();
+  }
+
+  _initHive() async{
+    box = Hive.box<Todo>('todos');
+  }
+
+  TodoRepositoryImpl(
+      {TodoDbService? todoDbService,
+        TodoFirestoreService? todoFirestoreService})
+      : _todoDbService = todoDbService ?? TodoDbService(),
+        _todoFirestoreService = todoFirestoreService ?? TodoFirestoreService();
+
+  @override
+  Future<void> addTodo(Todo todo) async{
+    await box.put(todo.id, todo);
+  }
+
+  @override
+  Future<void> deleteTodo(Todo todo) async{
+    await box.delete(todo.id);
+  }
+
+  @override
+  Future<void> updateTodo(Todo todo) async{
+  }
+
+  @override
+  Future<void> deleteAllTodos() async{
+    await box.clear();
+  }
+
+  @override
+  Future<List<Todo>> getAllTodos() {
+      return _todoFirestoreService
+          .getTodos()
+          .catchError((error) => _todoDbService.getTodos())
+          .then((value) => value);
+  }
+  
+}
